fc1c6190141e7d8628874701554a81f191a1ab0b
kadeel <kadeel@gmail.com>
2014-04-02 10:47:15 -0400

    fix math output for mediawiki (kind of)

--- lib/maruku/ext/math/to_mediawiki.rb
+++ lib/maruku/ext/math/to_mediawiki.rb
@@ -1,9 +1,8 @@
 module MaRuKu
   module Out
-    module Mediawiki
+    module MediaWiki
       def render_mathml(kind, tex)
-        # engine = get_setting(:html_math_engine)
-        engine = "itex2mml"
+        engine = get_setting(:html_math_engine)
         method = "convert_to_mathml_#{engine}"
         if self.respond_to? method
           mathml = self.send(method, kind, tex)
@@ -15,34 +14,24 @@ module MaRuKu
         convert_to_mathml_none(kind, tex)
       end
 
-      def to_wiki_inline_math
-        fix_latex("$#{self.math.strip}$")
+      def to_wiki_inline_math(context)
+        "$#{self.math.strip}$"
       end
 
-      def to_wiki_equation
+      def to_wiki_equation(context)
         if self.label
-          fix_latex("\\begin{equation}\n#{self.math.strip}\n\\label{#{self.label}}\\end{equation}\n")
+          "\\begin{equation}\n#{self.math.strip}\n\\label{#{self.label}}\\end{equation}\n"
         else
-          fix_latex("\\begin{displaymath}\n#{self.math.strip}\n\\end{displaymath}\n")
+          "\\\[ #{self.math.strip} \\\]\n\n"
         end
       end
 
-      def to_wiki_eqref
+      def to_wiki_eqref(context)
         "\\eqref{#{self.eqid}}"
       end
 
-      def to_wiki_divref
+      def to_wiki_divref(context)
         "\\ref{#{self.refid}}"
-      end
-
-      private
-
-      def fix_latex(str)
-        return str unless self.get_setting(:html_math_engine) == 'itex2mml'
-        s = str.gsub("\\mathop{", "\\operatorname{")
-        s.gsub!(/\\begin\{svg\}.*?\\end\{svg\}/m, " ")
-        s.gsub!("\\array{","\\itexarray{")
-        s.gsub("\\space{", "\\itexspace{")
       end
     end
   end
